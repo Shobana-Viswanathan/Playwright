9180f27810234a15a8967e5e96c876c2cbb72a69
Shobana <shobanav112@gmail.com>
2026-06-29 21:05:18 +0530

    Added

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   //fullyParallel: true,
 
   // CI Settings
-  forbidOnly: !!process.env.CI,
+ // forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [
